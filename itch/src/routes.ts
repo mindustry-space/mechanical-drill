a8fa041f2e17b572f7f5d4bc29984d8f0c4b85fe
Joel <joel@joel.tokyo>
2022-08-31 13:03:38 +1000

    feat: scrape uploads
    some contain build numbers

--- itch/src/routes.ts
+++ itch/src/routes.ts
@@ -2,8 +2,8 @@ import { Dataset, createCheerioRouter } from "crawlee";
 
 export const router = createCheerioRouter();
 
-router.addDefaultHandler(async ({ enqueueLinks, log }) => {
-  log.info(`enqueueing new URLs`);
+router.addDefaultHandler(async ({ enqueueLinks, log, request}) => {
+  log.info(request.loadedUrl);
   await enqueueLinks({
     globs: ["https://anuke.itch.io/mindustry/devlog/**"],
     label: "devlog",
@@ -13,10 +13,26 @@ router.addDefaultHandler(async ({ enqueueLinks, log }) => {
 router.addHandler("devlog", async ({ request, $, log }) => {
   log.info(request.loadedUrl);
 
+  let uploads = [];
+  $(".upload").each((i, upload) => {
+    let platforms = [];
+    $(upload).find(".download_platforms span").each((j, platform) => {
+      platforms.push($(platform).attr("title").replace("Download for ", ""))
+    })
+    
+    uploads.push({
+      date: $(upload).find(".upload_date").children("abbr").attr("title"),
+      name: $(upload).find(".name").text(),
+      platforms,
+      size: $(upload).find(".file_size").text(),
+    })
+  })
+
   await Dataset.pushData({
-    url: request.loadedUrl,
-    title: $("section.post_header").children("h1").text(),
     body: $("section.post_body").html(),
     published_at: $("div.post_meta").children("span").attr("title"),
+    title: $("section.post_header").children("h1").text(),
+    uploads,
+    url: request.loadedUrl,
   });
 });
